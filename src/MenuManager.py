3c6eba93db991b23dd669e652290b7fb2e0f8e61
aegroto <aegroto@protonmail.com>
2020-10-28 09:23:52 +0100

    module-based source restructuring

--- src/MenuManager.py
+++ src/MenuManager.py
@@ -3,7 +3,7 @@ import telegram, yaml, logging, traceback
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 
 from modules.pytg.Manager import Manager
-from modules.pytg.ModulesLoader import ModulesLoader
+from modules.pytg.load import manager, get_module_content_folder
 
 class MenuManager(Manager):
     @staticmethod
@@ -19,15 +19,16 @@ class MenuManager(Manager):
     def __init__(self):
         self.cached_reply_markup = { }
 
+    def __load_default_lang(self):
+        return manager("config").load_settings("menu", "lang")["default"]
+
     ###################
     # Menus interface #
     ###################
 
-    def create_reply_markup(self, menu_id, lang=None, meta={}):
+    def create_reply_markup(self, module_name, menu_id, lang=None, meta={}):
         if not lang:
-            config_manager = ModulesLoader.load_manager("config")
-            lang_settings = config_manager.load_settings_file("menu", "lang")
-            lang = lang_settings["default"]
+            lang = self.__load_default_lang()
 
         full_menu_id = "{}_{}".format(menu_id, lang)
 
@@ -38,18 +39,20 @@ class MenuManager(Manager):
         # Otherwise, create reply markup
         logging.info("Reply markup is not cached, loading...")
 
-        module_folder = ModulesLoader.get_module_content_folder("menu")
-
-        menu_data = yaml.safe_load(open("{}/formats/{}/{}.yaml".format(module_folder, lang, menu_id), "r", encoding="utf8"))
+        menu_format = self.__load_format(module_name, menu_id)
+        phrases = self.__load_phrases(module_name, menu_id, lang)
 
         menu_layout = []
 
         # Inline menu
-        if menu_data["type"] == "inline":
-            for row in menu_data["markup"]:
+        if menu_format["type"] == "inline":
+            for row in menu_format["markup"]:
                 menu_row = []
 
                 for button in row:
+                    # Retrieve phrase
+                    text = phrases[button["phrase"]]
+
                     # Retrieving callback data
                     callback_data = None
                     if "callback_data" in button.keys():
@@ -68,18 +71,14 @@ class MenuManager(Manager):
                         for meta_key in meta:
                             url = url.replace(meta_key, str(meta[meta_key]))
 
-                    menu_row.append(
-                        InlineKeyboardButton(button["text"],
-                        callback_data = callback_data,
-                        url = url)
-                    )
+                    menu_row.append(InlineKeyboardButton(text, callback_data = callback_data, url = url))
 
                 menu_layout.append(menu_row)
                 
             reply_markup = InlineKeyboardMarkup(menu_layout)
         else:
         # Reply menu
-            for row in menu_data["markup"]:
+            for row in menu_format["markup"]:
                 menu_row = []
 
                 for button in row:
@@ -89,24 +88,51 @@ class MenuManager(Manager):
                 
             reply_markup = ReplyKeyboardMarkup(menu_layout, resize_keyboard=True)
 
-        if menu_data["cacheable"]:
+        if menu_format["cacheable"]:
             logging.info("Saving reply markup in cache...")
             self.cached_reply_markup[full_menu_id] = reply_markup
 
         return reply_markup
+    
+    def __load_format(self, module_name, menu_id):
+        module_folder = get_module_content_folder(module_name)
 
-    def switch_menu(self, bot, chat_id, menu_id, message_id=None, force=False):
+        return yaml.safe_load(open("{}/menu/formats/{}.yaml".format(module_folder, menu_id), "r", encoding="utf8"))
+
+    def __load_phrases(self, module_name, menu_id, lang):
+        module_folder = get_module_content_folder(module_name)
+
+        return yaml.safe_load(open("{}/menu/phrases/{}/{}.yaml".format(module_folder, lang, menu_id), "r", encoding="utf8"))
+
+    def send_menu(self, bot, chat_id, module_name, menu_id, lang=None):
+        if not lang:
+            lang = self.__load_default_lang()
+
+        reply_markup = self.create_reply_markup(module_name, menu_id)
+
+        header_text = self.__load_phrases(module_name, menu_id, lang)["_header"]
+
+        bot.sendMessage(
+            chat_id = chat_id,
+            text = header_text,
+            reply_markup = reply_markup
+        )
+
+    def switch_menu(self, bot, chat_id, module_name, menu_id, message_id=None, force=False, lang=None):
         logging.info("Switching to menu {} for {} (message {})".format(menu_id, chat_id, message_id))
 
-        reply_markup = self.create_reply_markup(menu_id)
+        if not lang:
+            lang = self.__load_default_lang()
 
-        menu_headers = self.load_menu_headers()
+        reply_markup = self.create_reply_markup(module_name, menu_id)
+
+        header_text = self.__load_phrases(module_name, menu_id, lang)["_header"]
 
         if message_id:
             try:
                 bot.editMessageText(
                     chat_id = chat_id,
-                    text = menu_headers["{}".format(menu_id)],
+                    text = header_text,
                     message_id = message_id,
                     reply_markup = reply_markup
                 )
@@ -119,7 +145,7 @@ class MenuManager(Manager):
 
                     bot.sendMessage(
                         chat_id = chat_id,
-                        text = menu_headers["{}".format(menu_id)],
+                        text = header_text,
                         reply_markup = reply_markup
                     )
                 else:
@@ -128,16 +154,6 @@ class MenuManager(Manager):
         else:
             bot.sendMessage(
                 chat_id = chat_id,
-                text = menu_headers["{}".format(menu_id)],
+                text = header_text,
                 reply_markup = reply_markup
             )
-
-    def load_menu_headers(self, lang=None):
-        module_folder = ModulesLoader.get_module_content_folder("menu")
-
-        if not lang:
-            config_manager = ModulesLoader.load_manager("config")
-            lang_settings = config_manager.load_settings_file("menu", "lang")
-            lang = lang_settings["default"]
-
-        return yaml.safe_load(open("{}/text/{}/menu_headers.yaml".format(module_folder, lang), "r", encoding="utf8"))
